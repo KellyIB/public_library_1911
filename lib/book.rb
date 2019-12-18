88f16b6a987da1817186434aeeec0cacb76889d9
Kelly Bard <kellyibard@gmail.com>
2019-12-18 09:22:46 -0700

    Finish Iteration1, tests pass

---
+++ lib/book.rb
@@ -0,0 +1,25 @@
+class Book
+  attr_reader :title, :author, :publication_year
+
+  def initialize(book_info)
+    @title = book_info[:title]
+    @author_first_name = book_info[:author_first_name]
+    @author_last_name = book_info[:author_last_name]
+    @author = book_info[:author_first_name] + " " + book_info[:author_last_name]
+    @publication_year = book_info[:publication_date].slice(-4 .. -1)
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
